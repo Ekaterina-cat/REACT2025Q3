e7159215672eb9efeb0120b31bf2485baf9d1396
Katiaryna Dounar <ekaterinaaleks9@gmail.com>
2025-07-24 13:46:20 +0300

    fix: fix import error

--- src/tests/SearchView.test.tsx
+++ src/tests/SearchView.test.tsx
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 import { describe, it } from 'vitest';
-import { Search } from '../pages/search-view';
+import Search from '../pages/search-view';
 
 describe('SearchView Component', () => {
   it('render searchView component', () => {
