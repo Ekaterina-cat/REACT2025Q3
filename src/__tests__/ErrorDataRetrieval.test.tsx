bf4b35be06dd0963a12affc2da6fdf6719157d67
Katiaryna Dounar <ekaterinaaleks9@gmail.com>
2025-08-15 13:53:02 +0300

    refactor: delete tests

--- src/__tests__/ErrorDataRetrieval.test.tsx
+++
@@ -1,9 +0,0 @@
-import { ErrorDataRetrieval } from '@components/';
-import { render } from '@testing-library/react';
-import { describe, it } from 'vitest';
-
-describe('ErrorDataRetrieval Component', () => {
-  it('render errorDataRetrieval component', () => {
-    render(<ErrorDataRetrieval />);
-  });
-});
